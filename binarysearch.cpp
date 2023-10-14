8af0c300392899394e14eebd7a76e365f3ce982a
dhruvrahuja <dhruvrahuja09@gmail.com>
2023-10-14 23:59:39 +0530

    binary search

---
+++ binarysearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int binarysearch(int arr[],int n,int key)
+{
+    int start=0;
+    int end=n-1;
+    int mid=start+(end-start)/2;
+    while(start<=end)
+    {
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        if(key>arr[mid])
+        {
+            start=mid+1;
+        }
+        if(key<arr[mid])
+        {
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return -1;
+}
+int main()
+{
+    int key;
+    int arr1[5]={1,2,3,4,5};
+    cout<<"enter element ";
+    cin>>key;
+    cout<<"index of "<<key<<" is "<<binarysearch(arr1,5,key)<<endl;
+}
