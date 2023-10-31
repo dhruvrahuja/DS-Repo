542a948ecceca6b47b699fd835bb6efd301f0a60
dhruvrahuja <dhruvrahuja09@gmail.com>
2023-10-31 13:28:56 +0530

    -

---
+++ 20.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+    ~node() {
+        int value = this -> data;
+        cout << "memory is released for node with data " << value << endl;
+    }
+};
+void insert(node*& head, int position, int d)
+{
+    node* temp = head;
+    node* ans = new node(d);
+    int count = 0;
+    if(position == 1)
+    {
+        ans->next = head;
+        head = ans;
+        return;
+    }
+    while(count < position-2){
+        if (temp == NULL) {
+            cout << "Invalid position. Insertion not possible." << endl;
+            return;
+        }
+        temp = temp->next;
+        count++;
+    }
+    if(temp->next == NULL){
+        temp->next=ans;
+    }
+    else{
+        ans->next=temp->next;
+        temp->next=ans;
+    }
+}
+void remove(node*& head, int position)
+{
+    node* temp = head;
+    int count = 0;
+    if(position == 1)
+    {
+        node*store=head;
+        head=head->next;
+        delete store;
+        return;
+    }
+    while(count < position-2){
+        if (temp->next== NULL) {
+            cout << "Invalid position. Deletion not possible." << endl;
+            return;
+        }
+        temp = temp->next;
+        count++;
+    }
+    node*ans=temp->next;
+    temp->next=ans->next;
+    ans->next=NULL;
+    delete ans;
+}
+void print(node *&head)//traversing a linked list
+{
+    node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;    
+}
+void reverse(node*&head)
+{
+    if(head->next==NULL||head==NULL)
+    {
+        return;
+    }
+    node*temp=head->next;
+    reverse(temp);
+    head->next->next=head;
+    head->next=NULL;
+    head=temp;
+}
+int main()
+{
+    node* node1= new node(10);
+    node*head=node1;
+    node*tail=node1;
+    insert(head,2,3);
+    insert(head,3,4);
+    print(head);
+    cout<<" reversing now "<<endl;
+    reverse(head);
+    print(head);
+}
